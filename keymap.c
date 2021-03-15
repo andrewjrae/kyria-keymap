82d5f6ab1c76f0b1ac66a3cb8b654e8e3733f0a6
Andrew Rae <ajrae.nv@gmail.com>
2021-03-15 16:30:28 -0400

    Finished up the case modes docs.

--- keymap.c
+++ keymap.c
@@ -237,7 +237,7 @@ LT(_NAV,KC_SLSH), KC_R,    KC_S, KC_T, KC_H, KC_D,                              
 /* --------------- PROCESS RECORD --------------- */
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    // Process caps word for updates
+    // Process case modes
     if (!process_case_modes(keycode, record)) {
         return false;
     }

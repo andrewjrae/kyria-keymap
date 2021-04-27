07941503aea01ec3efa4c0b20e7612908322cf57
Andrew Rae <ajrae.nv@gmail.com>
2021-04-27 13:18:59 -0400

    Fixing the leader display macro.

--- features/leader.h
+++ features/leader.h
@@ -33,7 +33,7 @@ bool process_leader(uint16_t keycode, const keyrecord_t *record);
 #ifdef LEADER_DISPLAY_STR
 char *leader_display_str(void);
 
-#define OLED_LEADER_DISPLAY()                       \
+#define OLED_LEADER_DISPLAY() {                     \
     static uint16_t timer = 0;                      \
     if (is_leading()) {                             \
         oled_write_ln(leader_display_str(), false); \
@@ -43,6 +43,9 @@ char *leader_display_str(void);
         oled_write_ln(leader_display_str(), false); \
     }                                               \
     else {                                          \
+        // prevent it from ever looping around      \
+        timer = timer_read() - 200;                 \
         oled_write_ln("", false);                   \
-    }
+    }                                               \
+}
 #endif
